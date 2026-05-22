cc98aec9adae7b61b2049317a8c7f3677f97a90e
James Ahlborn <jtahlborn@yahoo.com>
2026-05-22 15:56:37 +0000

    add some enhancements around row size related errors
    git-svn-id: https://svn.code.sf.net/p/jackcess/code/jackcess/trunk@1456 f203690c-595d-4dc9-a70b-905162fa7fd2

--- src/main/java/com/healthmarketscience/jackcess/impl/TableImpl.java
+++ src/main/java/com/healthmarketscience/jackcess/impl/TableImpl.java
@@ -2297,8 +2297,7 @@ public class TableImpl implements Table, PropertyMaps.Owner
 
           int rowSize = rowData.remaining();
           if (rowSize > getFormat().MAX_ROW_SIZE) {
-            throw new InvalidValueException(withErrorContext(
-                    "Row size " + rowSize + " is too large"));
+            throw createTooLargeException(rowSize);
           }
 
           // get page with space
@@ -2551,8 +2550,7 @@ public class TableImpl implements Table, PropertyMaps.Owner
           keepRawVarValues);
 
       if (newRowData.limit() > getFormat().MAX_ROW_SIZE) {
-        throw new InvalidValueException(withErrorContext(
-                "Row size " + newRowData.limit() + " is too large"));
+        throw createTooLargeException(newRowData.limit());
       }
 
       if(!_indexDatas.isEmpty()) {
@@ -2891,8 +2889,7 @@ public class TableImpl implements Table, PropertyMaps.Owner
           } catch(BufferOverflowException e) {
             // if the data is too big for the buffer, then we have gone over
             // the max row size
-            throw new InvalidValueException(withErrorContext(
-                    "Row size " + buffer.limit() + " is too large"));
+            throw createTooLargeException(buffer.limit());
           }
         }
 
@@ -2909,6 +2906,11 @@ public class TableImpl implements Table, PropertyMaps.Owner
 
       // record where we stopped writing
       int eod = buffer.position();
+
+      if(buffer.remaining() < trailerSize) {
+        // we don't have enough space left for required trailer
+        throw createTooLargeException(eod + trailerSize);
+      }
 
       // insert padding if necessary
       padRowBuffer(buffer, minRowSize, trailerSize);
@@ -2931,6 +2933,13 @@ public class TableImpl implements Table, PropertyMaps.Owner
     nullMask.write(buffer);  //Null mask
     buffer.flip();
     return buffer;
+  }
+
+  private InvalidValueException createTooLargeException(int size) {
+    return new InvalidValueException(
+        withErrorContext(
+            "Row size " + size + " is too large (max " +
+            getFormat().MAX_ROW_SIZE + ")"));
   }
 
   /**
